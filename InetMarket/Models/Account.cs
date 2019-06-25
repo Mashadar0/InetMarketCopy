8875d2bae578581a1600b5b1f8577996e7e8c100
Dima Shagin <master150299@mail.ru>
2019-06-25 16:43:49 +0300

    Авторизацию на будущее

---
+++ InetMarket/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InetMarket.Models
+{
+    public class Account
+    {
+            [Required]
+            public string Name { get; set; }
+
+            [Required]
+            public string Password { get; set; }
+    }
+}
